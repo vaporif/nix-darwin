4b9ecf9a38a483ac7904ef6dce49ea194b0ce9c1
Dmytro Onypko <vaporif@gmail.com>
2025-04-14 10:38:05 +0100

    upd

--- system.nix
+++ system.nix
@@ -32,44 +32,6 @@
         cmd - 4 : open -a "Slack"
       '';
     };
-    aerospace = {
-      # enable = true;
-      settings = {
-        on-focused-monitor-changed = ["move-mouse monitor-lazy-center"];
-        gaps = {
-        };
-        mode.main.binding = {
-          alt-j = "focus left";
-          alt-u = "focus down";
-          alt-k = "focus up";
-          alt-l = "focus right";
-          alt-e = "macos-native-fullscreen";
-
-          cmd-1 = "workspace 1";
-          cmd-2 = "workspace 2";
-          cmd-3 = "workspace 3";
-          cmd-4 = "workspace 4";
-          cmd-5 = "workspace 5";
-          cmd-6 = "workspace 6";
-          cmd-7 = "workspace 7";
-          cmd-8 = "workspace 8";
-          cmd-9 = "workspace 9";
-          cmd-0 = "workspace 10";
-
-
-          alt-1 = "move-node-to-workspace 1";
-          alt-2 = "move-node-to-workspace 2";
-          alt-3 = "move-node-to-workspace 3";
-          alt-4 = "move-node-to-workspace 4";
-          alt-5 = "move-node-to-workspace 5";
-          alt-6 = "move-node-to-workspace 6";
-          alt-7 = "move-node-to-workspace 7";
-          alt-8 = "move-node-to-workspace 8";
-          alt-9 = "move-node-to-workspace 9";
-          alt-0 = "move-node-to-workspace 10";
-        };
-      };
-    };
   };
 
   security.pam.services.sudo_local.touchIdAuth = true;
