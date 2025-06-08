f726d13aa1513061c5939f4986bb0e240ced9157
Dmytro Onypko <vaporif@proton.me>
2025-06-08 12:15:16 +0100

    upd

--- nvim/lua/core/autocmds.lua
+++ nvim/lua/core/autocmds.lua
@@ -1,7 +1,3 @@
-local augroup = function(name)
-  return vim.api.nvim_create_augroup('nvim_' .. name, { clear = true })
-end
-
 vim.api.nvim_create_autocmd('TextYankPost', {
   group = vim.api.nvim_create_augroup('YankHighlight', { clear = true }),
   callback = function()
