64e87158054d214fa4c58b7a306533a39559b60d
Dmytro Onypko <vaporif@proton.me>
2025-06-13 22:08:27 +0100

    upd

--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -2,11 +2,12 @@ return {
   'folke/noice.nvim',
   event = 'VeryLazy',
   opts = {
-    cmdline = {
-      enabled = true,
-    },
-    messages = {
-      enabled = true,
+    presets = {
+      bottom_search = true,
+      command_palette = true,
+      long_message_to_split = true,
+      inc_rename = false,
+      lsp_doc_border = false,
     },
   },
   dependencies = {
