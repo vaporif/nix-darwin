93dcd06a87b1315476ec76cd789cecee347e2f56
Dmytro Onypko <vaporif@proton.me>
2025-09-08 13:17:55 +0100

    add golanglint

--- nvim/lua/plugins/lint.lua
+++ nvim/lua/plugins/lint.lua
@@ -5,6 +5,7 @@ return { -- Linting
     local lint = require 'lint'
     lint.linters_by_ft = {
       markdown = { 'markdownlint' },
+      go = { 'golangcilint' },
     }
 
     -- To allow other plugins to add linters to require('lint').linters_by_ft,
