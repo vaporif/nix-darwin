b44a7114d86b51beef63ca7660e99173d5a057f8
Dmytro Onypko <vaporif@proton.me>
2025-10-12 14:49:47 +0100

    update lualine

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,17 +1,38 @@
 return {
   'nvim-lualine/lualine.nvim',
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
-  opts = {
-    options = { theme = 'everforest' },
-    sections = {
-      lualine_a = { 'mode' },
-      lualine_b = { 'branch', 'diff', 'diagnostics' },
-      lualine_c = {
-        { 'filename', path = 3 },
+  dependencies = { 'nvim-tree/nvim-web-devicons', 'folke/trouble.nvim' },
+  opts = function(_, _)
+    local base_opts = {
+      options = { theme = 'everforest' },
+      sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch', 'diff', 'diagnostics' },
+        lualine_c = {
+          { 'filename', path = 3 },
+        },
+        lualine_x = { 'encoding', 'fileformat', 'filetype' },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' },
       },
-      lualine_x = { 'encoding', 'fileformat', 'filetype' },
-      lualine_y = { 'progress' },
-      lualine_z = { 'location' },
-    },
-  },
+    }
+
+    local trouble = require 'trouble'
+    local symbols = trouble.statusline {
+      mode = 'lsp_document_symbols',
+      groups = {},
+      title = false,
+      filter = { range = true },
+      format = '{kind_icon}{symbol.name:Normal}',
+      -- The following line is needed to fix the background color
+      -- Set it to the lualine section you want to use
+      hl_group = 'lualine_c_normal',
+    }
+
+    table.insert(base_opts.sections.lualine_c, {
+      symbols.get,
+      cond = symbols.has,
+    })
+
+    return base_opts
+  end,
 }
