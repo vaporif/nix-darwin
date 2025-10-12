03693ae45e8e37a9d6cedb14412ffca8e5e021e6
Dmytro Onypko <vaporif@proton.me>
2025-10-12 17:28:46 +0100

    undotree

--- nvim/lua/plugins/undotree.lua
+++ nvim/lua/plugins/undotree.lua
@@ -1,8 +1,7 @@
 return {
-  'jiaoshijie/undotree',
-  dependencies = 'nvim-lua/plenary.nvim',
-  config = true,
-  keys = { -- load the plugin only when using it's keybinding:
-    { '<leader>u', "<cmd>lua require('undotree').toggle()<cr>", desc = '[u]ndotree' },
-  },
+  'mbbill/undotree',
+  config = function()
+    vim.g.undotree_SetFocusWhenToggle = 1
+    vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
+  end,
 }
