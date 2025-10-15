639303a1cb1a2977944eef4bde0a12152549945b
Dmytro Onypko <vaporif@proton.me>
2025-10-15 03:37:19 +0100

    uupd

--- nvim/lua/core/mappings.lua
+++ nvim/lua/core/mappings.lua
@@ -15,7 +15,7 @@ vim.keymap.set('n', '<leader>ck', vim.diagnostic.setloclist, { desc = 'quic[k]fi
 
 vim.keymap.set('n', '<leader>/', 'gcc', { desc = 'toggle comment', remap = true })
 vim.keymap.set('v', '<leader>/', 'gc', { desc = 'toggle comment', remap = true })
-vim.keymap.set('n', '<leader>w', '<cmd>w!<CR>', { desc = 'write' })
+vim.keymap.set('n', '<leader>w', '<cmd>w!<CR>', { desc = '[w]rite' })
 vim.keymap.set('n', '<Leader>e', '<Cmd>Neotree float toggle reveal_force_cwd<CR>', { desc = 'n[e]otree' })
 vim.keymap.set('n', '<Leader>E', '<Cmd>Neotree float git_status toggle reveal<CR>', { desc = 'n[E]otree git' })
 -- Buffers
--- nvim/lua/plugins/undotree.lua
+++ nvim/lua/plugins/undotree.lua
@@ -2,6 +2,6 @@ return {
   'mbbill/undotree',
   config = function()
     vim.g.undotree_SetFocusWhenToggle = 1
-    vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
+    vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle, { desc = '[u]ndotree' })
   end,
 }
