8fd8aad86faca78d34b00ca3263592e67248b4cd
Dmytro Onypko <vaporif@proton.me>
2025-12-29 02:30:57 +0000

    devshell upd

--- home/default.nix
+++ home/default.nix
@@ -169,7 +169,7 @@ in {
   home.file =
     {
       ".envrc".text = ''
-        use flake github:vaporif/nix-devshells/683da1d6207cb130c611db98933e82b4a43f7900
+        use flake github:vaporif/nix-devshells/11b7a8af7649f404ad100374166a7a139dbff571
       '';
       # Stable symlink to Neovim runtime for .luarc.json
       ".local/share/nvim-runtime".source = "${pkgs.neovim-unwrapped}/share/nvim/runtime";
