e18a065194f847f756f7a39aa06c3ce2b2a07bec
Dmytro Onypko <vaporif@proton.me>
2025-12-29 03:15:52 +0000

    mapping update

--- nvim/lua/core/mappings.lua
+++ nvim/lua/core/mappings.lua
@@ -70,7 +70,7 @@ vim.keymap.set('n', 's', '<Plug>(SubversiveSubstitute)', {})
 vim.keymap.set('n', 'ss', '<Plug>(SubversiveSubstituteLine)', {})
 vim.keymap.set('n', 'S', '<Plug>(SubversiveSubstituteToEndOfLine)', {})
 vim.keymap.set('x', 's', '<Plug>(SubversiveSubstitute)', {})
-vim.api.nvim_set_keymap('i', 'ii', '<Esc>', { noremap = true })
+vim.keymap.set('i', 'ii', '<Esc>')
 
 -- Unbind hjkl since I use extend layer & colemak
 --
