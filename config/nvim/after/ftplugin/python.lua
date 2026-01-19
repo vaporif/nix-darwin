accad02b079b52973bcc87cf4b112b934dacaa7f
Dmytro Onypko <vaporif@proton.me>
2026-01-19 18:07:07 +0000

    python

---
+++ config/nvim/after/ftplugin/python.lua
@@ -0,0 +1,9 @@
+vim.keymap.set('n', '<leader>ci', function()
+  vim.lsp.buf.code_action { context = { only = { 'source.organizeImports' } }, apply = true }
+end, { buffer = 0, desc = 'organize [i]mports' })
+
+vim.keymap.set('n', '<leader>cr', '<cmd>!python %<CR>', { buffer = 0, desc = '[r]un file' })
+
+vim.opt_local.makeprg = 'pytest'
+vim.keymap.set('n', '<leader>ct', '<cmd>make<CR>', { buffer = 0, desc = '[t]est' })
+vim.keymap.set('n', '<leader>cT', '<cmd>!pytest -v %<CR>', { buffer = 0, desc = '[T]est file verbose' })
