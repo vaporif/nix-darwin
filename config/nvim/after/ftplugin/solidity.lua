235dc0f77212c78abc377302691982f516788427
Dmytro Onypko <vaporif@proton.me>
2026-01-19 18:06:47 +0000

    solidity

--- config/nvim/after/ftplugin/solidity.lua
+++ config/nvim/after/ftplugin/solidity.lua
@@ -1,4 +1,6 @@
 vim.opt_local.makeprg = 'forge build'
 vim.keymap.set('n', '<leader>cb', '<cmd>make<CR>', { buffer = 0, desc = '[b]uild' })
 vim.keymap.set('n', '<leader>ct', '<cmd>!forge test<CR>', { buffer = 0, desc = '[t]est' })
-vim.keymap.set('n', '<leader>cf', '<cmd>!forge fmt<CR>', { buffer = 0, desc = '[f]ormat' })
+vim.keymap.set('n', '<leader>cT', '<cmd>!forge test -vvvv<CR>', { buffer = 0, desc = '[T]est verbose' })
+vim.keymap.set('n', '<leader>cs', '<cmd>!forge snapshot<CR>', { buffer = 0, desc = 'gas [s]napshot' })
+vim.keymap.set('n', '<leader>cc', '<cmd>!forge coverage<CR>', { buffer = 0, desc = '[c]overage' })
