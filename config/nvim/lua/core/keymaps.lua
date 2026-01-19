f83858b2e12480e2ce24e2a87432950c9e15f7c7
Dmytro Onypko <vaporif@proton.me>
2026-01-19 19:23:45 +0000

    updates

--- config/nvim/lua/core/keymaps.lua
+++ config/nvim/lua/core/keymaps.lua
@@ -1,16 +1,22 @@
 -- Neotest
-vim.keymap.set('n', '<leader>ot', function()
+vim.keymap.set('n', '<leader>tt', function()
   require('neotest').run.run()
 end, { desc = 'run [t]est' })
-vim.keymap.set('n', '<leader>of', function()
+vim.keymap.set('n', '<leader>tf', function()
   require('neotest').run.run(vim.fn.expand '%')
 end, { desc = 'run [f]ile' })
-vim.keymap.set('n', '<leader>os', function()
+vim.keymap.set('n', '<leader>ts', function()
   require('neotest').summary.toggle()
 end, { desc = '[s]ummary' })
-vim.keymap.set('n', '<leader>op', function()
+vim.keymap.set('n', '<leader>tp', function()
   require('neotest').output_panel.toggle()
 end, { desc = 'output [p]anel' })
+vim.keymap.set('n', '<leader>tr', function()
+  require('neotest').run.run_last()
+end, { desc = '[r]e-run last' })
+vim.keymap.set('n', '<leader>tx', function()
+  require('neotest').run.stop()
+end, { desc = 'stop' })
 
 -- Debug (dap)
 vim.keymap.set('n', '<leader>dc', function()
@@ -57,21 +63,11 @@ vim.keymap.set('n', '<leader>p', function()
   local harpoon = require 'harpoon'
   harpoon.ui:toggle_quick_menu(harpoon:list())
 end, { desc = 'har[p]oon' })
-vim.keymap.set('n', '<leader>1', function()
-  require('harpoon'):list():select(1)
-end, { desc = 'mark [1]' })
-vim.keymap.set('n', '<leader>2', function()
-  require('harpoon'):list():select(2)
-end, { desc = 'mark [2]' })
-vim.keymap.set('n', '<leader>3', function()
-  require('harpoon'):list():select(3)
-end, { desc = 'mark [3]' })
-vim.keymap.set('n', '<leader>4', function()
-  require('harpoon'):list():select(4)
-end, { desc = 'mark [4]' })
-vim.keymap.set('n', '<leader>5', function()
-  require('harpoon'):list():select(5)
-end, { desc = 'mark [5]' })
+for i = 0, 9 do
+  vim.keymap.set('n', '<leader>' .. i, function()
+    require('harpoon'):list():select(i)
+  end, { desc = 'which_key_ignore' })
+end
 
 -- Outline
 vim.keymap.set('n', '<leader>co', '<cmd>Outline<CR>', { desc = '[o]utline' })
@@ -85,17 +81,17 @@ vim.keymap.set('n', '<leader>l', function()
 end, { desc = 'git [l]ogs' })
 
 -- Trouble
-vim.keymap.set('n', '<leader>tt', '<cmd>Trouble todo toggle<cr>', { desc = '[t]odo' })
-vim.keymap.set('n', '<leader>td', '<cmd>Trouble diagnostics toggle<cr>', { desc = '[d]iagnostics' })
-vim.keymap.set('n', '<leader>tb', '<cmd>Trouble diagnostics toggle filter.buf=0<cr>', { desc = '[b]uffer' })
-vim.keymap.set('n', '<leader>ts', '<cmd>Trouble symbols toggle focus=false<cr>', { desc = '[s]ymbols' })
-vim.keymap.set('n', '<leader>tl', '<cmd>Trouble lsp toggle focus=false win.position=right<cr>', { desc = '[L]SP Definitions / references / ...' })
-vim.keymap.set('n', '<leader>to', '<cmd>Trouble loclist toggle<cr>', { desc = 'l[o]ocation' })
-vim.keymap.set('n', '<leader>tq', '<cmd>Trouble qflist toggle<cr>', { desc = '[q]uickfix' })
-vim.keymap.set('n', ']t', function()
+vim.keymap.set('n', '<leader>bt', '<cmd>Trouble todo toggle<cr>', { desc = '[t]odo' })
+vim.keymap.set('n', '<leader>bd', '<cmd>Trouble diagnostics toggle<cr>', { desc = '[d]iagnostics' })
+vim.keymap.set('n', '<leader>bb', '<cmd>Trouble diagnostics toggle filter.buf=0<cr>', { desc = '[b]uffer' })
+vim.keymap.set('n', '<leader>bs', '<cmd>Trouble symbols toggle focus=false<cr>', { desc = '[s]ymbols' })
+vim.keymap.set('n', '<leader>bl', '<cmd>Trouble lsp toggle focus=false win.position=right<cr>', { desc = '[L]SP Definitions / references / ...' })
+vim.keymap.set('n', '<leader>bo', '<cmd>Trouble loclist toggle<cr>', { desc = 'l[o]ocation' })
+vim.keymap.set('n', '<leader>bq', '<cmd>Trouble qflist toggle<cr>', { desc = '[q]uickfix' })
+vim.keymap.set('n', ']b', function()
   require('trouble').next { skip_groups = true, jump = true }
 end, { desc = 'Next trouble item' })
-vim.keymap.set('n', '[t', function()
+vim.keymap.set('n', '[b', function()
   require('trouble').prev { skip_groups = true, jump = true }
 end, { desc = 'Previous trouble item' })
 
@@ -110,8 +106,8 @@ vim.keymap.set('n', ']e', function()
 end, { desc = 'Next error' })
 
 -- Code (generic)
-vim.keymap.set('n', '<leader>cf', '<cmd>DiffviewOpen<Cr>', { desc = 'di[f]f tool' })
-vim.keymap.set('n', '<leader>ci', '<cmd>AnsiEsc<Cr>', { desc = 'ans[i] escape' })
+vim.keymap.set('n', '<leader>cf', '<cmd>DiffviewOpen<CR>', { desc = 'di[f]f tool' })
+vim.keymap.set('n', '<leader>ci', '<cmd>AnsiEsc<CR>', { desc = 'ans[i] escape' })
 vim.keymap.set('n', '<leader>ck', vim.diagnostic.setloclist, { desc = 'quic[k]fix list' })
 
 vim.keymap.set('n', '<leader>/', 'gcc', { desc = 'toggle comment', remap = true })
@@ -119,16 +115,16 @@ vim.keymap.set('v', '<leader>/', 'gc', { desc = 'toggle comment', remap = true }
 vim.keymap.set('n', '<leader>w', '<cmd>w!<CR>', { desc = '[w]rite' })
 vim.keymap.set('n', '<leader>e', '<cmd>Neotree float toggle reveal_force_cwd<CR>', { desc = 'n[e]otree' })
 
-vim.keymap.set('n', '<leader><Tab>', '<C-w>w', { noremap = true, desc = '[tab] pane' })
+vim.keymap.set('n', '<leader><Tab>', '<C-w>w', { desc = 'next pane' })
 
-vim.keymap.set('n', '<leader>sv', ':vsplit<CR>', { noremap = true, desc = '[v]ertically' })
-vim.keymap.set('n', '<leader>sh', ':split<CR>', { noremap = true, desc = '[h]orizontally' })
+vim.keymap.set('n', '<leader>sv', '<cmd>vsplit<CR>', { desc = '[v]ertically' })
+vim.keymap.set('n', '<leader>sh', '<cmd>split<CR>', { desc = '[h]orizontally' })
 
 -- Buffer navigation
 vim.keymap.set('n', '<S-h>', '<cmd>bprevious<CR>', { desc = 'Prev buffer' })
 vim.keymap.set('n', '<S-l>', '<cmd>bnext<CR>', { desc = 'Next buffer' })
 
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>', { desc = 'Clear search highlight' })
 -- yanky
 vim.keymap.set({ 'n', 'x' }, 'p', '<Plug>(YankyPutAfter)')
 vim.keymap.set({ 'n', 'x' }, 'P', '<Plug>(YankyPutBefore)')
@@ -177,9 +173,9 @@ vim.keymap.set('x', 's', '<Plug>(SubversiveSubstitute)')
 vim.keymap.set('i', 'ii', '<Esc>')
 
 -- Unbind hjk since I use extend layer & colemak (l is used by Flash)
-vim.keymap.set({ 'n', 'v', 'o' }, 'h', '<Nop>', { noremap = true })
-vim.keymap.set({ 'n', 'v', 'o' }, 'j', '<Nop>', { noremap = true })
-vim.keymap.set({ 'n', 'v', 'o' }, 'k', '<Nop>', { noremap = true })
+vim.keymap.set({ 'n', 'v', 'o' }, 'h', '<Nop>')
+vim.keymap.set({ 'n', 'v', 'o' }, 'j', '<Nop>')
+vim.keymap.set({ 'n', 'v', 'o' }, 'k', '<Nop>')
 
 -- ; -> :
 vim.keymap.set({ 'n', 'v', 'x' }, ';', ':')
--- config/nvim/lua/plugins/neotest.lua
+++ config/nvim/lua/plugins/neotest.lua
@@ -4,12 +4,21 @@ return {
     'nvim-neotest/nvim-nio',
     'nvim-lua/plenary.nvim',
     'nvim-treesitter/nvim-treesitter',
+    'fredrikaverpil/neotest-golang',
+    'nvim-neotest/neotest-python',
+    'marilari88/neotest-vitest',
   },
   cmd = 'Neotest',
   config = function()
     require('neotest').setup {
       adapters = {
         require 'rustaceanvim.neotest',
+        require 'neotest-golang',
+        require 'neotest-python' {
+          dap = { justMyCode = false },
+          pytest_discover_instances = true,
+        },
+        require 'neotest-vitest',
       },
     }
   end,
