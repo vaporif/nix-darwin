d4870ba8ed1932aaadbe654fea9b8fe16be828da
Dmytro Onypko <vaporif@proton.me>
2026-01-19 18:29:44 +0000

    update

--- config/nvim/after/ftplugin/go.lua
+++ config/nvim/after/ftplugin/go.lua
@@ -2,14 +2,8 @@ vim.keymap.set('n', '<leader>ci', function()
   vim.lsp.buf.code_action { context = { only = { 'source.organizeImports' }, diagnostics = {} }, apply = true }
 end, { buffer = 0, desc = 'organize [i]mports' })
 
-vim.keymap.set('n', '<leader>ce', function()
-  vim.lsp.buf.code_action { context = { only = { 'refactor.rewrite.fillStruct' }, diagnostics = {} }, apply = true }
-end, { buffer = 0, desc = 'fill struct' })
-
-vim.keymap.set('n', '<leader>ct', function()
-  vim.lsp.buf.code_action { context = { only = { 'gopls.tidy' }, diagnostics = {} }, apply = true }
-end, { buffer = 0, desc = 'go mod [t]idy' })
-
 vim.opt_local.makeprg = 'go build ./...'
 vim.keymap.set('n', '<leader>cb', '<cmd>make<CR>', { buffer = 0, desc = '[b]uild' })
+vim.keymap.set('n', '<leader>ct', '<cmd>!go test ./...<CR>', { buffer = 0, desc = '[t]est' })
 vim.keymap.set('n', '<leader>cr', '<cmd>!go run .<CR>', { buffer = 0, desc = '[r]un' })
+vim.keymap.set('n', '<leader>cm', '<cmd>!go mod tidy<CR>', { buffer = 0, desc = '[m]od tidy' })
