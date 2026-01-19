dcbfb1a59c25f35a0d62493b9c70486236dec768
Dmytro Onypko <vaporif@proton.me>
2026-01-19 17:57:36 +0000

    go maps

--- config/nvim/after/ftplugin/go.lua
+++ config/nvim/after/ftplugin/go.lua
@@ -2,5 +2,14 @@ vim.keymap.set('n', '<leader>ci', function()
   vim.lsp.buf.code_action { context = { only = { 'source.organizeImports' } }, apply = true }
 end, { buffer = 0, desc = 'organize [i]mports' })
 
+vim.keymap.set('n', '<leader>ce', function()
+  vim.lsp.buf.code_action { context = { only = { 'refactor.rewrite.fillStruct' } }, apply = true }
+end, { buffer = 0, desc = 'fill struct' })
+
+vim.keymap.set('n', '<leader>ct', function()
+  vim.lsp.buf.code_action { context = { only = { 'gopls.tidy' } }, apply = true }
+end, { buffer = 0, desc = 'go mod [t]idy' })
+
 vim.opt_local.makeprg = 'go build ./...'
 vim.keymap.set('n', '<leader>cb', '<cmd>make<CR>', { buffer = 0, desc = '[b]uild' })
+vim.keymap.set('n', '<leader>cr', '<cmd>!go run .<CR>', { buffer = 0, desc = '[r]un' })
