5442a6b50c7d18f0e29b6899ad984619df104069
Dmytro Onypko <vaporif@proton.me>
2026-01-24 21:30:39 +0000

    misc reorder

--- config/nvim/lua/plugins/misc.lua
+++ config/nvim/lua/plugins/misc.lua
@@ -2,12 +2,70 @@ return {
   { 'neovim/nvim-lspconfig', event = 'BufReadPre' },
   { 'tpope/vim-sleuth', event = 'BufReadPre' },
   {
+    'rmagatti/auto-session',
+    lazy = false,
+    opts = {
+      suppressed_dirs = { '~/', '~/Repos', '~/Downloads', '/' },
+      no_restore_cmds = { 'Neotree float reveal' },
+    },
+  },
+
+  {
+    'folke/noice.nvim',
+    event = 'VeryLazy',
+    dependencies = { 'MunifTanjim/nui.nvim' },
+    opts = {
+      presets = {
+        bottom_search = true,
+        command_palette = true,
+        long_message_to_split = true,
+        inc_rename = false,
+        lsp_doc_border = false,
+      },
+    },
+  },
+  { 'chentoast/marks.nvim', event = 'VeryLazy', opts = {} },
+  {
+    'folke/todo-comments.nvim',
+    event = 'VimEnter',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    opts = { signs = false },
+  },
+
+  {
+    'gbprod/yanky.nvim',
+    keys = {
+      { 'p', '<Plug>(YankyPutAfter)', mode = { 'n', 'x' }, desc = 'Put after' },
+      { 'P', '<Plug>(YankyPutBefore)', mode = { 'n', 'x' }, desc = 'Put before' },
+      { 'gp', '<Plug>(YankyGPutAfter)', mode = { 'n', 'x' }, desc = 'GPut after' },
+      { 'gP', '<Plug>(YankyGPutBefore)', mode = { 'n', 'x' }, desc = 'GPut before' },
+      { '<c-p>', '<Plug>(YankyPreviousEntry)', desc = 'Yanky previous' },
+      { '<c-n>', '<Plug>(YankyNextEntry)', desc = 'Yanky next' },
+    },
+    opts = {
+      preserve_cursor_position = {},
+    },
+  },
+  {
     'svermeulen/vim-subversive',
     keys = {
       { 's', '<Plug>(SubversiveSubstitute)', desc = 'Substitute' },
       { 'ss', '<Plug>(SubversiveSubstituteLine)', desc = 'Substitute line' },
       { 'S', '<Plug>(SubversiveSubstituteToEndOfLine)', desc = 'Substitute to EOL' },
       { 's', '<Plug>(SubversiveSubstitute)', mode = 'x', desc = 'Substitute' },
+    },
+  },
+
+  { 'chrisgrieser/nvim-early-retirement', event = 'VeryLazy', config = true },
+  { 'sindrets/diffview.nvim', cmd = { 'DiffviewOpen', 'DiffviewFileHistory', 'DiffviewClose' } },
+
+  {
+    'folke/lazydev.nvim',
+    ft = 'lua',
+    opts = {
+      library = {
+        { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
+      },
     },
   },
   {
@@ -24,26 +82,7 @@ return {
       vim.keymap.set('n', '<leader>cA', '<cmd>BaleiaColorize<cr>', { desc = '[A]nsi colorize' })
     end,
   },
-  { 'sindrets/diffview.nvim', cmd = { 'DiffviewOpen', 'DiffviewFileHistory', 'DiffviewClose' } },
-  {
-    'chrisgrieser/nvim-early-retirement',
-    config = true,
-    event = 'VeryLazy',
-  },
-  {
-    'gbprod/yanky.nvim',
-    keys = {
-      { 'p', '<Plug>(YankyPutAfter)', mode = { 'n', 'x' }, desc = 'Put after' },
-      { 'P', '<Plug>(YankyPutBefore)', mode = { 'n', 'x' }, desc = 'Put before' },
-      { 'gp', '<Plug>(YankyGPutAfter)', mode = { 'n', 'x' }, desc = 'GPut after' },
-      { 'gP', '<Plug>(YankyGPutBefore)', mode = { 'n', 'x' }, desc = 'GPut before' },
-      { '<c-p>', '<Plug>(YankyPreviousEntry)', desc = 'Yanky previous' },
-      { '<c-n>', '<Plug>(YankyNextEntry)', desc = 'Yanky next' },
-    },
-    opts = {
-      preserve_cursor_position = {},
-    },
-  },
+
   {
     'tidalcycles/vim-tidal',
     ft = 'tidal',
@@ -53,43 +92,5 @@ return {
       vim.g.tidal_boot = vim.fn.expand '~/.config/tidal/Tidal.ghci'
       vim.g.tidal_sc_enable = false
     end,
-  },
-  {
-    'rmagatti/auto-session',
-    lazy = false,
-    opts = {
-      suppressed_dirs = { '~/', '~/Repos', '~/Downloads', '/' },
-      no_restore_cmds = { 'Neotree float reveal' },
-    },
-  },
-  { 'chentoast/marks.nvim', event = 'VeryLazy', opts = {} },
-  {
-    'folke/todo-comments.nvim',
-    event = 'VimEnter',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    opts = { signs = false },
-  },
-  {
-    'folke/lazydev.nvim',
-    ft = 'lua',
-    opts = {
-      library = {
-        { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
-      },
-    },
-  },
-  {
-    'folke/noice.nvim',
-    event = 'VeryLazy',
-    opts = {
-      presets = {
-        bottom_search = true,
-        command_palette = true,
-        long_message_to_split = true,
-        inc_rename = false,
-        lsp_doc_border = false,
-      },
-    },
-    dependencies = { 'MunifTanjim/nui.nvim' },
   },
 }
