f446b47bd759a70cd81d3ebcdf16ce6404d1c652
Dmytro Onypko <vaporif@proton.me>
2026-01-24 21:28:31 +0000

    simplify

--- config/nvim/lua/plugins/misc.lua
+++ config/nvim/lua/plugins/misc.lua
@@ -57,10 +57,39 @@ return {
   {
     'rmagatti/auto-session',
     lazy = false,
-    ---enables autocomplete for opts
     opts = {
       suppressed_dirs = { '~/', '~/Repos', '~/Downloads', '/' },
       no_restore_cmds = { 'Neotree float reveal' },
     },
+  },
+  { 'chentoast/marks.nvim', event = 'VeryLazy', opts = {} },
+  {
+    'folke/todo-comments.nvim',
+    event = 'VimEnter',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    opts = { signs = false },
+  },
+  {
+    'folke/lazydev.nvim',
+    ft = 'lua',
+    opts = {
+      library = {
+        { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
+      },
+    },
+  },
+  {
+    'folke/noice.nvim',
+    event = 'VeryLazy',
+    opts = {
+      presets = {
+        bottom_search = true,
+        command_palette = true,
+        long_message_to_split = true,
+        inc_rename = false,
+        lsp_doc_border = false,
+      },
+    },
+    dependencies = { 'MunifTanjim/nui.nvim' },
   },
 }
