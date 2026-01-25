a47a0aa7c60bda54ceea9175a5dfaa24e00cbf07
Dmytro Onypko <vaporif@proton.me>
2026-01-25 12:06:27 +0000

    cleanup

--- config/nvim/after/ftplugin/just.lua
+++
@@ -1,3 +0,0 @@
-vim.opt_local.makeprg = 'just'
-vim.keymap.set('n', '<leader>cr', '<cmd>make<CR>', { buffer = 0, desc = '[r]un default' })
-vim.keymap.set('n', '<leader>cl', '<cmd>!just --list<CR>', { buffer = 0, desc = '[l]ist recipes' })
