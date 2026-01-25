f1b072c404b487c2e15ddb7ef7ab72c23037a420
Dmytro Onypko <vaporif@proton.me>
2026-01-25 06:22:29 +0000

    yanky refactor

--- config/nvim/lua/plugins/misc.lua
+++ config/nvim/lua/plugins/misc.lua
@@ -32,62 +32,6 @@ return {
     opts = { signs = false },
   },
 
-  {
-    'gbprod/yanky.nvim',
-    event = 'VeryLazy',
-    keys = {
-      { 'p', '<Plug>(YankyPutAfter)', mode = { 'n', 'x' }, desc = 'Put after' },
-      { 'P', '<Plug>(YankyPutBefore)', mode = { 'n', 'x' }, desc = 'Put before' },
-      { 'gp', '<Plug>(YankyGPutAfter)', mode = { 'n', 'x' }, desc = 'GPut after' },
-      { 'gP', '<Plug>(YankyGPutBefore)', mode = { 'n', 'x' }, desc = 'GPut before' },
-      { '<c-p>', '<Plug>(YankyPreviousEntry)', desc = 'Yanky previous' },
-      { '<c-n>', '<Plug>(YankyNextEntry)', desc = 'Yanky next' },
-    },
-    opts = {
-      preserve_cursor_position = { enabled = true },
-    },
-  },
-  {
-    'gbprod/substitute.nvim',
-    keys = {
-      {
-        's',
-        function()
-          require('substitute').operator()
-        end,
-        desc = 'Substitute',
-      },
-      {
-        'ss',
-        function()
-          require('substitute').line()
-        end,
-        desc = 'Substitute line',
-      },
-      {
-        'S',
-        function()
-          require('substitute').eol()
-        end,
-        desc = 'Substitute to EOL',
-      },
-      {
-        's',
-        function()
-          require('substitute').visual()
-        end,
-        mode = 'x',
-        desc = 'Substitute',
-      },
-    },
-    opts = {
-      on_substitute = function(event)
-        require('yanky.integration').substitute()(event)
-      end,
-      highlight_substituted_text = { enabled = true, timer = 300 },
-    },
-  },
-
   { 'chrisgrieser/nvim-early-retirement', event = 'VeryLazy', config = true },
   { 'sindrets/diffview.nvim', cmd = { 'DiffviewOpen', 'DiffviewFileHistory', 'DiffviewClose' } },
 
