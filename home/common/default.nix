2cb6d1b9aad84f3b8a7e4317ff1b60dda5fca387
Dmytro Onypko <vaporif@proton.me>
2026-02-23 02:46:22 +0900

    ignore redb

--- home/common/default.nix
+++ home/common/default.nix
@@ -129,7 +129,7 @@ in {
 
     git = {
       enable = true;
-      ignores = [".direnv" ".serena" ".claude" "CLAUDE.md" ".serena.bak" ".claude.bak" ".envrc.bak" "CLAUDE.md.bak"];
+      ignores = [".direnv" ".serena" ".claude" "CLAUDE.md" ".serena.bak" ".claude.bak" ".envrc.bak" ".parry-guard.redb" "CLAUDE.md.bak"];
       settings = {
         user = {
           inherit (userConfig.git) name email;
