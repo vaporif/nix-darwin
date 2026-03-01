ac8437a9f684b674f09067ad9d22d497730476a0
Dmytro Onypko <vaporif@proton.me>
2026-03-01 14:14:19 +0000

    add full scanmoed

--- home/common/default.nix
+++ home/common/default.nix
@@ -194,6 +194,7 @@ in {
     parry = {
       enable = true;
       package = parry.packages.${pkgs.stdenv.hostPlatform.system}.default;
+      scanMode = "full";
       hfTokenFile = "/run/secrets/hf-token-scan-injection";
       ignorePaths = ["${homeDir}/Repos/parry" userConfig.configPath];
     };
